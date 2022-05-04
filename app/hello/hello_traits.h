afae5a22153844798dab8c193936db1f574b47e6
lucena <lucena@lisha.ufsc.br>
2022-05-04 10:41:39 -0300

    Reference to solution E4(Preemptive Scheduling)

--- app/hello/hello_traits.h
+++ app/hello/hello_traits.h
@@ -12,7 +12,7 @@ template<> struct Traits<Build>: public Traits_Tokens
     static const unsigned int MODE = LIBRARY;
     static const unsigned int ARCHITECTURE = RV32;
     static const unsigned int MACHINE = RISCV;
-    static const unsigned int MODEL = SiFive_E;
+    static const unsigned int MODEL = SiFive_E; 
     static const unsigned int CPUS = 1;
     static const unsigned int NODES = 1; // (> 1 => NETWORKING)
     static const unsigned int EXPECTED_SIMULATION_TIME = 60; // s (0 => not simulated)
